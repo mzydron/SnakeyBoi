36373e6056688d626f81cb2f06f1f79138db2595
mzydron <zydronmaciej@gmail.com>
2019-05-16 18:40:44 +0200

    SemiStable

---
+++ Assets/Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public int score; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<Text>().text = "Score: " + score.ToString();
+        //var StatementText = textTransform.GetComponent<Text>();
+    }
+}
